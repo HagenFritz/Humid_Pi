70a004a5ef11be9db21761cc95485ad4a33b03ed
Dev Govindji <47125945+Dev-94@users.noreply.github.com>
2019-09-17 23:03:42 -0500

    Update humidity_logger.py

--- Code/humidity_logger.py
+++ Code/humidity_logger.py
@@ -8,12 +8,16 @@ DHT_PIN = 4
 try: 
 	f = open('/home/pi/humidity.csv', 'a+')
 	if os.stat('/home/pi/humidity.csv').st_size == 0:
-		f.write('Date,TIme, Temperature, Humidity#r#n')
+		f.write('Date,Time,Temperature,Humidity\r\n')
 except:
-	passs
+	pass
 
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
 	if humidity is not None and temperature is not None:
-		f.write('{0}, {1}, {2:0.1f}*C, {3:0.1f}*C, {3:0.1f}%
+		f.write('{0}, {1}, {2:0.1f}*C, {3:0.1f}%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity))
+	else:
+		print("Failed to retrieve data from humidity sensor")
+		
+	time.sleep(30)
